6416ccb4284ab7f5c84c5e408df3d30e745df9b0
johnkdunyo <johnkdunyo@gmail.com>
2023-07-27 10:46:19 +0000

    code cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import HappyClients from "@/components/homePage/HappyClients";
-import TrustedBy from "@/components/homePage/TrustedBy";
-import OurCultureSection from "@/components/homePage/OurCultureSection";
-import CertifiedSection from "@/components/homePage/CertifiedSection";
-import HeroSection from "@/components/homePage/HeroSection";
 import SiteLayout from "@/components/Layout/SiteLayout";
-import MissionVission from "@/components/homePage/MissionVission2";
-import YouCanCountOnUs from "@/components/homePage/YouCanCountOnUs";
-import MissionVissionMobile from "@/components/homePage/MissionVissionMobile";
+import AboutSection from "@/components/digitalPayments/AboutSection";
+import CustomDigitalSection from "@/components/digitalPayments/CustomDigitalSection";
+import DiscoverBusinessSection from "@/components/digitalPayments/DiscoverBusinessSection";
+import HeroSection from "@/components/digitalPayments/HeroSection";
+import MissionVissionSection from "@/components/digitalPayments/MissionVissionSection";
+import NumberSection from "@/components/digitalPayments/NumberSection";
+import OurExpetiseSection from "@/components/digitalPayments/OurExpetiseSection";
+import WorkPOSSection from "@/components/digitalPayments/WorkPOSSection";
+import React from "react";
 
-export default function Home() {
+const Bdp = () => {
   return (
-    <SiteLayout pageName={"BSL Home"}>
+    <SiteLayout pageName={"Digital Payment"}>
       <>
         <HeroSection />
-        <YouCanCountOnUs />
-        <MissionVission />
-        <MissionVissionMobile />
-        <CertifiedSection />
-        <OurCultureSection />
-        <TrustedBy />
-        <HappyClients />
+        <AboutSection />
+        <DiscoverBusinessSection />
+        {/* <CustomDigitalSection /> */}
+        <WorkPOSSection />
+        <OurExpetiseSection />
+        <MissionVissionSection />
+        <NumberSection />
       </>
     </SiteLayout>
   );
-}
+};
+
+export default Bdp;
